610a6e7fbc4f685f92b8d8034141f7c3fb7d72cf
Jason Wiker <Jason.wiker@vercel.com>
2022-12-28 12:47:54 -0700

    .

--- apps/web/app/head.tsx
+++ apps/web/app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title></title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </>
   );
 }
