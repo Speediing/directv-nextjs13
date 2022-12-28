0cd27eb250a0a13c7dbe091821f16be697694b9b
Jason Wiker <Jason.wiker@vercel.com>
2022-12-27 17:19:59 -0700

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     domains: ['directv.com','www.directv.com'],
   },
   experimental: {
-    runtime: 'edge', 
+    runtime: 'experimental-edge', 
   },
   reactStrictMode: true,
   transpilePackages: ["ui"],
